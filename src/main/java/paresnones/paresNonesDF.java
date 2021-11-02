a4b72e5c020052fc26c762d97b262863503cfc6c
dferfer235 <dferfer235@g.educaand.es>
2021-11-02 12:16:26 +0100

    Pedir pares o nones a uno de los 2 jugadores

--- src/main/java/paresnones/paresNonesDF.java
+++ src/main/java/paresnones/paresNonesDF.java
@@ -1,5 +1,6 @@
 package paresnones;
 
+import java.util.Random;
 import javax.swing.JOptionPane;
 
 /**
@@ -10,7 +11,21 @@ public class paresNonesDF {
     public static void main(String[] args) {
         int dedosJ1, dedosJ2;
 
+        Random jugadorAleatorio = new Random();
+
         System.out.println("Bienvenido a Pares o Nones");
+
+        JOptionPane.showMessageDialog(null, "Un jugador escojerá pares o nones");
+
+        int jugadorElegido = jugadorAleatorio.nextInt(2) + 1;
+
+        JOptionPane.showMessageDialog(null, "Jugador " + jugadorElegido + " te toca escoger");
+
+        String paresNonesS = JOptionPane.showInputDialog(null,
+                "Jugador "+jugadorElegido+" ¿Pares o nones?\n"
+                + "1-. Pares\n"
+                + "2-. Nones");
+        int paresNones = Integer.parseInt(paresNonesS);
 
         String dedosTexto = JOptionPane.showInputDialog(null,
                 "Jugador 1, escribe los dedos que vas a sacar");
