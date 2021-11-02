9d03d62b2e1ef1f3d9f3fd2fa747cfa8d59d2a2c
dferfer235 <dferfer235@g.educaand.es>
2021-11-02 12:06:44 +0100

    Pedir cantidad de dedos  a los jugadores

--- src/main/java/paresnones/paresNonesDF.java
+++ src/main/java/paresnones/paresNonesDF.java
@@ -1,14 +1,25 @@
-
 package paresnones;
+
+import javax.swing.JOptionPane;
 
 /**
  * @author davidfdez
  */
 public class paresNonesDF {
 
-    
     public static void main(String[] args) {
-        
+        int dedosJ1, dedosJ2;
+
+        System.out.println("Bienvenido a Pares o Nones");
+
+        String dedosTexto = JOptionPane.showInputDialog(null,
+                "Jugador 1, escribe los dedos que vas a sacar");
+        dedosJ1 = Integer.parseInt(dedosTexto);
+
+        dedosTexto = JOptionPane.showInputDialog(null,
+                "Jugador 2, escribe los dedos que vas a sacar");
+        dedosJ2 = Integer.parseInt(dedosTexto);
+
     }
 
 }
