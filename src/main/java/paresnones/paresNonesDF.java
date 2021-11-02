e116ceaea52fab8576c76a2a6e94e0179282083a
dferfer235 <dferfer235@g.educaand.es>
2021-11-02 12:35:45 +0100

    Repeticion del programa a eleccion del usuario

--- src/main/java/paresnones/paresNonesDF.java
+++ src/main/java/paresnones/paresNonesDF.java
@@ -14,67 +14,82 @@ public class paresNonesDF {
         int paresNones;
         String paresNonesS;
 
+        boolean repetirJuego = true;
+
         Random jugadorAleatorio = new Random();
 
         System.out.println("Bienvenido a Pares o Nones");
 
         JOptionPane.showMessageDialog(null, "Un jugador escojerá pares o nones");
 
-        int jugadorElegido = jugadorAleatorio.nextInt(2) + 1;
+        do {
+            int jugadorElegido = jugadorAleatorio.nextInt(2) + 1;
 
-        switch (jugadorElegido) {
-            case 1:
-                JOptionPane.showMessageDialog(null, "Jugador " + jugadorElegido + " te toca escoger");
+            switch (jugadorElegido) {
+                case 1:
+                    JOptionPane.showMessageDialog(null, "Jugador " + jugadorElegido + " te toca escoger");
 
-                paresNonesS = JOptionPane.showInputDialog(null,
-                        "Jugador " + jugadorElegido + " ¿Pares o nones?\n"
-                        + "1-. Pares\n"
-                        + "2-. Nones");
-                paresNones = Integer.parseInt(paresNonesS);
+                    paresNonesS = JOptionPane.showInputDialog(null,
+                            "Jugador " + jugadorElegido + " ¿Pares o nones?\n"
+                            + "1-. Pares\n"
+                            + "2-. Nones");
+                    paresNones = Integer.parseInt(paresNonesS);
 
-                if (paresNones == 1) {
-                    j1Pares = true;
-                    j2Pares = false;
-                } else {
-                    j2Pares = true;
-                    j1Pares = false;
-                }
-                break;
+                    if (paresNones == 1) {
+                        j1Pares = true;
+                        j2Pares = false;
+                    } else {
+                        j2Pares = true;
+                        j1Pares = false;
+                    }
+                    break;
 
-            case 2:
-                JOptionPane.showMessageDialog(null, "Jugador " + jugadorElegido + " te toca escoger");
+                case 2:
+                    JOptionPane.showMessageDialog(null, "Jugador " + jugadorElegido + " te toca escoger");
 
-                paresNonesS = JOptionPane.showInputDialog(null,
-                        "Jugador " + jugadorElegido + " ¿Pares o nones?\n"
-                        + "1-. Pares\n"
-                        + "2-. Nones");
-                paresNones = Integer.parseInt(paresNonesS);
+                    paresNonesS = JOptionPane.showInputDialog(null,
+                            "Jugador " + jugadorElegido + " ¿Pares o nones?\n"
+                            + "1-. Pares\n"
+                            + "2-. Nones");
+                    paresNones = Integer.parseInt(paresNonesS);
 
-                if (paresNones == 1) {
-                    j2Pares = true;
-                    j1Pares = false;
-                } else {
-                    j1Pares = true;
-                    j2Pares = false;
-                }
-                break;
-        }
+                    if (paresNones == 1) {
+                        j2Pares = true;
+                        j1Pares = false;
+                    } else {
+                        j1Pares = true;
+                        j2Pares = false;
+                    }
+                    break;
+            }
 
-        String dedosTexto = JOptionPane.showInputDialog(null,
-                "Jugador 1, escribe los dedos que vas a sacar");
-        dedosJ1 = Integer.parseInt(dedosTexto);
+            String dedosTexto = JOptionPane.showInputDialog(null,
+                    "Jugador 1, escribe los dedos que vas a sacar");
+            dedosJ1 = Integer.parseInt(dedosTexto);
 
-        dedosTexto = JOptionPane.showInputDialog(null,
-                "Jugador 2, escribe los dedos que vas a sacar");
-        dedosJ2 = Integer.parseInt(dedosTexto);
+            dedosTexto = JOptionPane.showInputDialog(null,
+                    "Jugador 2, escribe los dedos que vas a sacar");
+            dedosJ2 = Integer.parseInt(dedosTexto);
 
-        int totalDedos = dedosJ1 + dedosJ2;
+            int totalDedos = dedosJ1 + dedosJ2;
 
-        if (j1Pares == true && totalDedos % 2 == 0) {
-            JOptionPane.showMessageDialog(null, "!Jugador 1 ha ganado!");
-        } else {
-            JOptionPane.showMessageDialog(null, "!Jugador 2 ha ganado!");
-        }
+            if (j1Pares == true && totalDedos % 2 == 0) {
+                JOptionPane.showMessageDialog(null, "!Jugador 1 ha ganado!");
+            } else {
+                JOptionPane.showMessageDialog(null, "!Jugador 2 ha ganado!");
+            }
+            String seleccionRepeticion = JOptionPane.showInputDialog(null,
+                    "¿Quieres jugar otra ronda?\n"
+                    + "1-. Si\n"
+                    + "2-. No");
+            int seleccion = Integer.parseInt(seleccionRepeticion);
+            
+            if (seleccion == 2) {
+                repetirJuego = false;
+                System.out.println("Saliendo...");
+            }
+
+        } while (repetirJuego == true);
 
     }
 
